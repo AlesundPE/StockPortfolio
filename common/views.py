0266308b0608ddb15f4dc81b73d96cbd08a2986c
Yiran Tan <43208955+AlesundPE@users.noreply.github.com>
2021-05-11 22:09:43 +0800

    Add files via upload

--- common/views.py
+++ common/views.py
@@ -3,6 +3,9 @@ from .models import Customer
 from .models import Stock
 from django.contrib import messages
 from .forms import StockForm
+import datetime
+
+
 
 def listCustomers(request):
     
@@ -11,7 +14,7 @@ def listCustomers(request):
     return render(request, 'customer.html', {'customers': qs })
 
 def home(request):
-    import requests
+    '''import requests
     import json
 
     if request.method == 'POST':
@@ -24,7 +27,8 @@ def home(request):
         return render(request, 'home.html', {'api': api })
 
     else:
-        return render(request, 'home.html', {'ticker': "Welcome to the Short-term Investor Sidekick!"})
+    '''
+    return render(request, 'home.html', {'ticker': "Welcome to the Short-term Investor Sidekick!"})
 
     # pk_fc1636b4895648e499e805830cd8430a
     
@@ -32,68 +36,62 @@ def home(request):
 def about(request):
     return render(request, 'about.html', {})
 
-
-'''
-    tickerSymbol = models.CharField(max_length = 10,null=True)
-    latestPrice = models.CharField(max_length = 20,null=True)
-    previousClose = models.CharField(max_length = 20,null=True)
-    marketCap = models.CharField(max_length = 20,null=True)
-    ytdChange = models.CharField(max_length = 20,null=True)
-    week52High = models.CharField(max_length = 20,null=True)
-    week52Low = models.CharField(max_length = 20,null=True)
-'''
-
+# pk_fc1636b4895648e499e805830cd8430a
 def addStock(request):
     import requests
     import json
 
-    if request.method == 'POST':
-        
-        form = StockForm(request.POST or None)
+    dateToday = datetime.datetime.now().strftime("%Y-%m-%d")
 
+    if request.method == 'POST':
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+request.POST.get("tickerSymbol","")+
+        "/quote?token=pk_fc1636b4895648e499e805830cd8430a&filter=symbol,latestPrice,previousClose,marketCap,ytdChange,week52High,week52Low")
+        
+        api = json.loads(api_request.content)
+
+        form = StockForm({"tickerSymbol":api['symbol'],"latestPrice":api['latestPrice'],"previousClose":api['previousClose'],
+        "marketCap":api['marketCap'],"ytdChange":round(api['ytdChange'],5),"week52High":api['week52High'],"week52Low":api['week52Low']})
         if form.is_valid():
             form.save()
             messages.success(request, ("Stock has been added"))
-
-            '''
-            api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+request.POST.get('tickerSymbol')+"/quote?token=pk_fc1636b4895648e499e805830cd8430a")
-            api = json.loads(api_request.content)
-            Stock=form.save(commit=False)
-            Stock.latestPrice=api.latestPrice
-            '''
-
-
-
-            return redirect('addStock')
-
+        else:
+            print(form.errors)
+        return redirect(addStock)
+            
         
-
-
+  
     else:
-        
+        #&filter=symbol,latestPrice,previousClose,marketCap,ytdChange,week52High,week52Low
         ticker = Stock.objects.all()
-        tickerList = []
+        updateInstance = requests.get("https://cloud.iexapis.com/stable/stock/amc/quote?token=pk_fc1636b4895648e499e805830cd8430a&filter=isUSMarketOpen")
+        mkt = json.loads(updateInstance.content)
 
-        for ticker_item in ticker:
+        if mkt["isUSMarketOpen"] == "true":
+            for tickerSymbol in ticker:
+                api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+str(tickerSymbol)+
+                "/quote?token=pk_fc1636b4895648e499e805830cd8430a&filter=symbol,latestPrice,previousClose,marketCap,ytdChange,week52High,week52Low")
+                try:
+                    api = json.loads(api_request.content)
+                    #tickerList.append(api)
+                    '''
+                    form = StockForm({"tickerSymbol":api['symbol'],"latestPrice":api['latestPrice'],"previousClose":api['previousClose'],
+                    "marketCap":api['marketCap'],"ytdChange":round(api['ytdChange'],5),"week52High":api['week52High'],"week52Low":api['week52Low']})
+                    form.save()
+                    '''
+                    item = Stock.objects.get(tickerSymbol=api['symbol'])
+                    item.latestPrice=api['latestPrice']
+                    item.previousClose=api['previousClose']
+                    item.marketCap=api['marketCap']
+                    item.ytdChange=round(api['ytdChange'],5)
+                    item.week52High=api['week52High']
+                    item.week52Low=api['week52Low']
+                    item.save()
 
-            api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+str(ticker_item)+"/quote?token=pk_fc1636b4895648e499e805830cd8430a")
-            try:
-                api = json.loads(api_request.content)
-                tickerList.append(api)
-                '''
-                Stock.objects.filter(tickerSymbol=str(ticker_item)).create(latestPrice=api.latestPrice)
-                Stock.objects.filter(tickerSymbol=str(ticker_item)).create(previousClose=api.previousClose)
-                Stock.objects.filter(tickerSymbol=str(ticker_item)).create(marketCap=api.marketCap)
-                Stock.objects.filter(tickerSymbol=str(ticker_item)).create(ytdChange=api.ytdChange)
-                Stock.objects.filter(tickerSymbol=str(ticker_item)).create(week52High=api.week52High)
-                Stock.objects.filter(tickerSymbol=str(ticker_item)).create(week52Low=api.week52Low)
-                Stock.save()
-                '''
-            except Exception as e:
-                api = "Error"
+                except Exception as e:
+                    api = "Error"
+
         
-        form = StockForm
-        return render(request, 'addStock.html', {'ticker': ticker, 'tickerList': tickerList})
+        return render(request, 'addStock.html', {'ticker': ticker})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
@@ -102,4 +100,9 @@ def delete(request, stock_id):
     return redirect(addStock)
 
 def tvChart(request):
-    return render(request, 'index.html', {})
+    import requests
+    if request.method == 'POST':
+        tickerSymbol = request.POST['tickerSymbol']
+        return render(request, 'index.html', {'tickerSymbol': tickerSymbol})
+    else:
+        return render(request, 'index.html', {})
